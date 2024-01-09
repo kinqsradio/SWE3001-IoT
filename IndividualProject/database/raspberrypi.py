79ed318a6b88a3c865b85c3bc4de3dd16975f455
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2024-01-10 09:55:01 +1100

    Update raspberrypi.py

--- IndividualProject/database/raspberrypi.py
+++ IndividualProject/database/raspberrypi.py
@@ -43,6 +43,7 @@ try:
         while True:
             if ser.in_waiting > 0:
                 line = ser.readline().decode('utf-8').rstrip()
+                print(line)
                 parts = line.split(',')
                 print(parts)
                 try:
