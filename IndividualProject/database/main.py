3103383f847417a827bd157b1d12d0806d54fd91
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2024-01-10 11:01:55 +1100

    0.1

--- IndividualProject/database/main.py
+++ IndividualProject/database/main.py
@@ -7,43 +7,6 @@ import datetime
 import time
 from dbConfig import config
 
-app = Flask(__name__)
-@app.route('/api/v1/sensor-data', methods=['GET'])
-def get_sensor_data():
-    try:
-        connection = mysql.connector.connect(**config)
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute(f"SELECT * FROM SensorData ORDER BY id DESC LIMIT 10")
-            records = cursor.fetchall()
-            print("Total number of rows in SensorData is: ", cursor.rowcount)
-            print("\nPrinting each SensorData record")
-            
-            data = []
-            for row in records:
-                data.append({
-                    'DHTTemperature': row[1],
-                    'Humidity': row[2],
-                    'LM35Temperature': row[3],
-                    'Time': row[4]
-                })
-            
-            # Only keep the recent 200 data
-            if len(data) > 200:
-                data = data[-200:]
-                
-            return jsonify(data)
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
-    
 def create_database(cursor, db_name):
     try:
         cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name};")
